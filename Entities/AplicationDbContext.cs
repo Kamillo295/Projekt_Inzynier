dab3ae2e1bbc6ed572fc91ee427df56eeabb5ae3
Jakub <jakub.kleczek.72@gmail.com>
2025-11-02 21:28:01 +0100

    fixed prev commit

--- Entities/AplicationDbContext.cs
+++ Entities/AplicationDbContext.cs
@@ -6,6 +6,8 @@ namespace Projekcik.Entities
     {
         public DbSet<Users> Zadownicy { get; set; }
         public DbSet<Team> Druzyny { get; set; }
+        public DbSet<Robots> Roboty { get; set; }
+        public DbSet<Categories> Kategorie { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=RobotyDB;Trusted_Connection=True;");
---
+++ Migrations/20251102202650_AddMigrations.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projekcik.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMigrations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Kategorie",
+                columns: table => new
+                {
+                    IdKategorii = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NazwaKategorii = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kategorie", x => x.IdKategorii);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roboty",
+                columns: table => new
+                {
+                    IdRobota = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdKategorii = table.Column<int>(type: "int", nullable: false),
+                    NazwaRobota = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roboty", x => x.IdRobota);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Kategorie");
+
+            migrationBuilder.DropTable(
+                name: "Roboty");
+        }
+    }
+}
