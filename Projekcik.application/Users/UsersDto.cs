bb1e825a0d8d5421e216a31b43359892d88397f3
Kamillo295 <kammil2005@gmail.com>
2025-11-26 15:03:36 +0100

    CSS

--- Projekcik.application/Users/UsersDto.cs
+++ Projekcik.application/Users/UsersDto.cs
@@ -1,15 +1,33 @@
-﻿namespace Projekcik.application.Users
+﻿using System.ComponentModel.DataAnnotations; // Pamiętaj o tym usingu!
+
+namespace Projekcik.application.Users
 {
     public class UsersDto
     {
-        public int IdZawodnika { get; set; } 
-        public string? Imie { get; set; } = default!;
-        public string? Nazwisko { get; set; } = default!;
-        public string? NumerTelefonu { get; set; } = default!;
-        public string? Haslo { get; set; } = default!;
+        public int IdZawodnika { get; set; }
+
+        [Display(Name = "Imię")] // To zmieni nagłówek w tabeli i etykietę w formularzu
+        public string Imie { get; set; } = default!;
+
+        [Display(Name = "Nazwisko")]
+        public string Nazwisko { get; set; } = default!;
+
+        [Display(Name = "Numer telefonu")]
+        public string NumerTelefonu { get; set; } = default!;
+
+        [Display(Name = "Hasło")]
+        public string Haslo { get; set; } = default!;
+
+        [Display(Name = "Adres e-mail")]
         public string? Email { get; set; }
+
+        [Display(Name = "Rozmiar koszulki")]
         public string? RozmiarKoszulki { get; set; }
+
+        [Display(Name = "Wiek zawodnika")]
         public int? Wiek { get; set; }
+
+        [Display(Name = "Kod pocztowy")]
         public string? KodPocztowy { get; set; }
     }
 }
